e3052c2ea5496a7ea263407e125f4cdb9e3644e8
luatnc <luatncth2007024@fpt.edu.vn>
2024-09-05 23:23:27 +0700

    khoi-tao-du-an

---
+++ resources/views/client/home/home.blade.php
@@ -0,0 +1,35 @@
+  @extends('client.layouts.index')
+  @section('body-client')
+
+      <!-- ======= Hero Section ======= -->
+    <section id="hero" class="d-flex align-items-center">
+      <!-- <div class="bg-home">
+          <video playsinline autoplay muted loop>
+              <source src="polina.webm" type="video/webm">
+              <source src="https://videos.pexels.com/video-files/8306452/8306452-uhd_4096_2160_25fps.mp4" type="video/mp4">
+              <source src="https://videos.pexels.com/video-files/7568747/7568747-hd_1920_1080_25fps.mp4" type="video/mp4">
+              <source src="../assets/video/video_background.mp4" type="video/mp4">
+          </video>
+      </div> -->
+
+      <div class="container position-relative text-center text-lg-start" data-aos="zoom-in" data-aos-delay="100">
+        <div class="row">
+          <div class="col-lg-8">
+          <h1>PHÒNG KHÁM ĐA KHOA  <br><span>MEDITEC</span></h1>
+          <!-- <h2>Mua sắm thả ga, không lo về giá!</h2> -->
+
+            <!-- <div class="btns">
+              <a href="#menu" class="btn-menu animated fadeInUp scrollto">Our Menu</a>
+              <a href="#book-a-table" class="btn-book animated fadeInUp scrollto">Book a Table</a>
+            </div> -->
+          </div>
+          <div class="col-lg-4 d-flex align-items-center justify-content-center position-relative" data-aos="zoom-in" data-aos-delay="200">
+            <a href="https://www.youtube.com/watch?v=GlrxcuEDyF8" class="glightbox play-btn"></a>
+          </div>
+
+        </div>
+      </div>
+    </section><!-- End Hero -->
+      <main id="main">
+
+  @endsection
